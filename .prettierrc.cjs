cc3498a6d1df9f4881e5b202fe2d5ebb3e8ce9b4
ccppoo <shmishmi79@gmail.com>
2024-03-23 18:33:59 +0900

    lint options

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: "all",
+  importOrder: [
+    "^(^react$|@react|react|^recoil$)",
+    "^@mui/(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^[./]"
+  ],
+
+    plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+}
