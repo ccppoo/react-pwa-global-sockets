b9caf0decfdcf2cc8ab3b4c5ee517bde778849b8
ccppoo <shmishmi79@gmail.com>
2024-03-23 18:40:59 +0900

    eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,37 @@
+import * as path from 'path';
+
+const thisDir = path.resolve(__dirname);
+
 module.exports = {
   settings: {
     react: {
-     version: "detect",
+      version: 'detect',
     },
   },
- }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:json/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-restricted-imports': ['error', { patterns: ['@mui/*/*/*'] }],
+  },
+};
